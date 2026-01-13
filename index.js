ccc4a2147d353434d4f4d7f3bb3dc60481515687
itsmesk17 <78195791+itsmesk17@users.noreply.github.com>
2026-01-13 05:33:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
+import { readFileSync } from 'node:fs';
 import { getInput, setOutput, setFailed, summary, getBooleanInput } from "@actions/core";
 import { context } from "@actions/github";
 
 try {
     const htmlFile = getInput("html_file");
     const jobSummary = getBooleanInput("job_summary");
-
     const { sha, repo: { owner, repo } } = context;
-
-    const fs = require('fs');
-    const htmlContent = fs.readFileSync(htmlFile, 'utf8');
+    console.log('htmlFile', htmlFile);
+    const htmlContent = readFileSync(htmlFile, 'utf8');
 
     if (jobSummary) {
         summary
